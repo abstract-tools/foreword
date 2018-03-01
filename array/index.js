ebf3b6b59ed7feddef454231f6ab3f4821072acd
Brandon Chartier <brandon.chartier@gmail.com>
2018-02-28 22:53:05 -0500

    adding functions to array index

--- array/index.js
+++ array/index.js
@@ -1,5 +1,6 @@
 const append = require('./append');
 const concat = require('./concat');
+const drop = require('./drop');
 const every = require('./every');
 const filter = require('./filter');
 const find = require('./find');
@@ -7,15 +8,20 @@ const includes = require('./includes');
 const isEmpty = require('./isEmpty');
 const length = require('./length');
 const map = require('./map');
+const max = require('./max');
+const min = require('./min');
 const reduce = require('./reduce');
 const reverse = require('./reverse');
 const slice = require('./slice');
 const some = require('./some');
 const sort = require('./sort');
+const take = require('./take');
+const unique = require('./unique');
 
 module.exports = {
   append,
   concat,
+  drop,
   every,
   filter,
   find,
@@ -23,9 +29,13 @@ module.exports = {
   isEmpty,
   length,
   map,
+  max,
+  min,
   reduce,
   reverse,
   slice,
   some,
-  sort
+  sort,
+  take,
+  unique
 };
