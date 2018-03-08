cac88731d67172d2db8c01c431e2af15da91e487
Brandon Chartier <brandon.chartier@gmail.com>
2018-03-08 05:02:26 -0500

    object map bugfix

--- object/map.js
+++ object/map.js
@@ -3,7 +3,7 @@ const curry = require('../function/curry');
 // map :: Object k (a -> b) -> Object k v -> Object k v
 module.exports = curry((spec, obj) => {
   return Object.assign({}, obj, Object.keys(spec).reduce((acc, key) => {
-    return (!obj[key]) ? acc : Object.assign({}, acc, {
+    return (obj[key] === undefined) ? acc : Object.assign({}, acc, {
       [key]: spec[key](obj[key])
     });
   }, {}));
