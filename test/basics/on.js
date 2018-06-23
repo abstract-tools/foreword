25084b5686a0d007b4c1baa4519efbed2b240cb3
Brandon Chartier <brandon.chartier@gmail.com>
2018-06-23 16:33:22 -0400

    fixing tests for new on signature

--- test/basics/on.js
+++ test/basics/on.js
@@ -2,7 +2,7 @@ const test = require('tape')
 const { on } = require('../../index')
 
 test('on', t => {
-  const result = on((x, y) => x === y, x => x.length)('hey', 'now')
+  const result = on(x => y => x === y, x => x.length)('hey', 'now')
 
   t.same(result, true)
   t.end()
